c70846de95be29a68f170f81f252774be7e04c46
Atte Salo <attesalo@msn.com>
2020-01-08 16:49:45 +0200

    Full on refactor

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,51 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Clock.scss';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
+const Clock = () => {
+  const refDate = new Date();
+  const [ hours, setHours ] = useState(refDate.getHours());
+  const [ minutes, setMinutes ] = useState(refDate.getMinutes());
+  const [ seconds, setSeconds ] = useState(refDate.getSeconds());
 
-    this.state = this.updateTime();
-  }
-
-  updateTime() {
-    const date = new Date();
-    return {
-      hours: date.getHours(),
-      minutes: date.getMinutes(),
-      seconds: date.getSeconds()
-    };
-  }
-
-  getHandAngle(value, hand) {
-    const max = hand === 'hours' ? 12 : 60;
-    const timeValue = hand === 'hours' && value > 12 ? value - 12 : value;
-    const rotation = Math.round(timeValue / max * 360 - 90);
-    
-    return rotation;
-  }
-
-  getHandStyle(hand) {
-    return {
-      transform: `translateX(50%) rotate(${this.getHandAngle(this.state[hand], hand)}deg)`
-    }
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      this.setState(this.updateTime());
-    }, 1000)
+  const tick = () => {
+    const newRefDate = new Date();
+    setHours(newRefDate.getHours());
+    setMinutes(newRefDate.getMinutes());
+    setSeconds(newRefDate.getSeconds());
   };
-  
-  render() {
-    return (
-      <div className="clock">
-        <span className="clock__hours" style={this.getHandStyle('hours')}/>
-        <span className="clock__minutes" style={this.getHandStyle('minutes')}/>
-        <span className="clock__seconds" style={this.getHandStyle('seconds')}/>
-      </div>
-    )
+
+  useEffect(() => {
+    const interval = setInterval(tick, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  const getHandStyle = (value, hours) => {
+    const max = hours ? 12 : 60;
+    const timeValue = hours && value > 12 ? value - 12 : value;
+    const rotation = Math.round(timeValue / max * 360 - 90);
+
+    const style = {
+      transform: `translateX(50%) rotate(${rotation}deg)`
+    };
+
+    if (timeValue === 0) style.transition = 'none'; // Dumb fix for 59 -> 0 rotation transition
+
+    return style;
   }
+
+  return (
+    <div className="clock">
+      <span className="clock__hours" style={ getHandStyle(hours, true) }/>
+      <span className="clock__minutes" style={ getHandStyle(minutes) }/>
+      <span className="clock__seconds" style={ getHandStyle(seconds) }/>
+    </div>
+  )
 }
 
 export default Clock;
